bd9f49362c2eafe71a5cb516b2ef89deb8c4e7da
DuduCopaja <dudu-copaja@hotmail.com>
2024-06-25 18:08:01 -0300

    Removendo arquivos do Vue E instalando fontes

--- src/App.vue
+++ src/App.vue
@@ -5,44 +5,19 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+       
   </header>
 
   <main>
-    <TheWelcome />
-  </main>
+   <h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima blanditiis tenetur quidem fugiat nulla voluptatem nihil dolores aliquam, quos minus veritatis odio, voluptates deserunt hic culpa at, molestiae accusamus. Nihil?
+   </h1>
+   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+body {
+  font-family: 'Inter', sans-serif !important;
 }
 </style>
 <script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import './assets/main.css'
 import App from './App.vue'
 import { createApp } from 'vue'
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
+import '@fontsource/chakra-petch';
+import '@fontsource-variable/inter';
 
 const app = createApp(App);
 
