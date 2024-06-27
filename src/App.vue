cf7c72d2e25fa3e137eba1530a09781690c3de82
Hugo Antonio <hugoaft@outlook.com>
2024-06-27 18:23:28 -0300

    atualização site

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,14 @@ import Home from './components/Home.vue'
 <style>
 body {
   font-family: 'Inter', sans-serif !important;
+  background-color: #313241;;
 }
 
 .chakra {
   font-family: 'Chakra Petch', sans-serif !important;
+}
+.title, .subtitle {
+  color: white !important
 }
 </style>
 <script>
