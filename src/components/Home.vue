75e4bfd66652a4f140ca469d1b15a634c57b3434
DuduCopaja <dudu-copaja@hotmail.com>
2024-06-27 18:34:05 -0300

    Não mudei nada (eu acho)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,64 +1,64 @@
 <template>
-<nav class="navbar" role="navigation" aria-label="main navigation" style="background-color: #222434;" >
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io"></a>
-    <figure class="image is-640x480">
-        <img src="../assets/logos/OIG2.zZtZI-removebg-preview-removebg-preview.png" style="max-height: 100px;" >
-    </figure>
-
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-
-      
-
-
+  <nav class="navbar" role="navigation" aria-label="main navigation" style="background-color: #222434;" >
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io"></a>
+      <figure class="image is-640x480">
+          <img src="../assets/logos/OIG2.zZtZI-removebg-preview-removebg-preview.png" style="max-height: 100px;" >
+      </figure>
+  
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
-
-        <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-light" style="background-color: #0ff29f;">
-            <strong>Cadastre-se</strong>
-          </a>
-          <a class="button is-light" style="background-color: #0ff29f">
-            Log in
-          </a>
+  
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+  
+        
+  
+  
+      </div>
+  
+          <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-light" style="background-color: #0ff29f;">
+              <strong>Cadastre-se</strong>
+            </a>
+            <a class="button is-light" style="background-color: #0ff29f">
+              Log in
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</nav>
-
-<section class="section is-large">
-  <h1 class="title chakra">Texto do Site*</h1>
-  <h2 class="subtitle">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ducimus eius tenetur sit omnis ipsam, tempore iusto atque molestias, in hic vel vero saepe ad voluptatibus quam dolores maxime odit! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis necessitatibus aperiam repellendus eaque voluptatem mollitia nemo, quis error explicabo, ad consectetur omnis tempora excepturi aspernatur alias veritatis maxime labore doloremque.
-  </h2>
-</section>
-
-<section class="section is-large">
-  <h1 class="title">Imagens de Jogos*</h1>
-  <h2 class="subtitle">
-    <img src="" alt="">
-  </h2>
-</section>
-
-<footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      <strong>My Games List</strong> by <a href="https://jgthms.com">Travequeiros do TI</a>.
-      The source code is licensed
-      <a href=#>MIT</a>. The website content is licensed
-      <a href=#></a>.
-    </p>
-  </div>
-</footer>
-</template>
+  </nav>
+  
+  <section class="section is-large">
+    <h1 class="title chakra">Texto do Site*</h1>
+    <h2 class="subtitle">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ducimus eius tenetur sit omnis ipsam, tempore iusto atque molestias, in hic vel vero saepe ad voluptatibus quam dolores maxime odit! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis necessitatibus aperiam repellendus eaque voluptatem mollitia nemo, quis error explicabo, ad consectetur omnis tempora excepturi aspernatur alias veritatis maxime labore doloremque.
+    </h2>
+  </section>
+  
+  <section class="section is-large">
+    <h1 class="title">Imagens de Jogos*</h1>
+    <h2 class="subtitle">
+      <img src="" alt="">
+    </h2>
+  </section>
+  
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>My Games List</strong> by <a href="https://jgthms.com">Travequeiros do TI</a>.
+        The source code is licensed
+        <a href=#>MIT</a>. The website content is licensed
+        <a href=#></a>.
+      </p>
+    </div>
+  </footer>
+  </template>
